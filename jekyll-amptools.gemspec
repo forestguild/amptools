a06e5f76f57f15f449d2ff83e98b1fee10156c14
Nikita Chernyi <osi@rakshazi.me>
2019-04-05 12:34:45 +0300

    bugfix render

--- jekyll-amptools.gemspec
+++ jekyll-amptools.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = 'jekyll-amptools'
-    s.version     = '1.0.1'
+    s.version     = '1.0.2'
     s.date        = '2019-04-04'
     s.summary     = "AMP tools for jekyll, fork of juusaw/amp-jekyll"
     s.description = "AMP tools for jekyll, fork of  juusaw/amp-jekyll"
--- lib/jekyll/AmpPage.rb
+++ lib/jekyll/AmpPage.rb
@@ -4,8 +4,8 @@ module Jekyll
     class AmpPage < Jekyll::Page
         include Jekyll::AmpFilter
         def initialize(site, base, dir, post)
-            body = (Liquid::Template.parse post.content).render site.site_payload
             self.data = post.data.clone
+            self.content = self.getContent(post, site)
             self.data['layout'] = 'amp'
             @site = site
             @base = base
@@ -13,9 +13,16 @@ module Jekyll
             @url = dir
             @name = 'index.html'
             self.process(@name)
-            self.content = amp_images(amp_iframes(body))
-            self.data['body'] = amp_images(amp_iframes(body))
+            self.data['body'] = self.content
             self.data['canonical_url'] = post.url
+        end
+
+        def getContent(page, site)
+            raw = (Liquid::Template.parse page.content, :error_mode => :warn).render site.site_payload, :strict_variables => false, :strict_filters => false
+            #Note about gsub: some jekyll versions ignore error_mode and strict_* options, so we need to remove that messages manually :(
+            html = Kramdown::Document.new(raw).to_html.gsub('Liquid error: internal','')
+
+            amp_images(amp_iframes(html))
         end
     end
     class AmpGenerator < Generator
